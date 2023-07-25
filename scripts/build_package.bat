447a489cbbe87e0322195f1bd6edb056fd8f0dca
Ulibos <49131389+Ulibos@users.noreply.github.com>
2023-07-25 21:59:15 +0300

    Add safeguard to package builder.

--- scripts/build_package.bat
+++ scripts/build_package.bat
@@ -1,4 +1,19 @@
 @echo OFF
 pushd %~dp0\..
+
+if exist .\ndf_parse\bin\ndf.dll goto build
+if exist %LocalAppData%\tree-sitter\lib\ndf.dll goto prebuild
+
+echo ! ERROR !
+echo No %%LocalAppData%%\tree-sitter\lib\ndf.dll or .\ndf_parse\bin\ndf.dll
+echo was found! You have to provide the library before building. Please refer to
+echo https://github.com/Ulibos/ndf-parse#development and docs in general for
+echo more info.
+exit /b 1
+
+:prebuild
+copy %LocalAppData%\tree-sitter\lib\ndf.dll .\ndf_parse\bin\ndf.dll
+
+:build
 python -m build -o build\package
 popd
